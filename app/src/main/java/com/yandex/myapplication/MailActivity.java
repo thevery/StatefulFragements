707eea87d0d20e70a9664c78a65a7233794815f3
Ildar Karimov <thevery@yandex-team.ru>
2015-09-19 04:52:12 +0300

    initial version

---
+++ app/src/main/java/com/yandex/myapplication/MailActivity.java
@@ -0,0 +1,51 @@
+package com.yandex.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+/**
+ * Created by thevery on 18/09/15.
+ */
+public class MailActivity extends AppCompatActivity
+        implements MainListFragment.Callbacks, MainAccountFragment.Callbacks {
+    public static final String TAG1 = "TAG1";
+    public static final String TAG2 = "TAG2";
+
+    private boolean twoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        twoPane = findViewById(R.id.container_2) != null;
+        if (savedInstanceState == null) {
+            final FragmentManager fm = getSupportFragmentManager();
+            final FragmentTransaction transaction = fm.beginTransaction();
+
+            transaction.add(R.id.container_1, new MainListFragment(), TAG1);
+            if (twoPane) {
+                transaction.add(R.id.container_2, new ItemDetailFragment(), TAG2);
+            }
+
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void onItemSelected(String id) {
+        Toast.makeText(this, "MailActivity.onItemSelected: " + id, Toast.LENGTH_SHORT).show();
+
+        final ItemDetailFragment fragment = (ItemDetailFragment) getSupportFragmentManager().findFragmentByTag(TAG2);
+        fragment.setText("set from activity: " + id);
+    }
+
+    @Override
+    public void onAccountSelected(String id) {
+        final FragmentManager fm = getSupportFragmentManager();
+        final MainListFragment fragment = (MainListFragment) fm.findFragmentByTag(TAG1);
+        fragment.setAccount(id);
+    }
+}
---
+++ app/src/main/java/com/yandex/myapplication/MainAccountFragment.java
@@ -0,0 +1,67 @@
+package com.yandex.myapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yandex.myapplication.dummy.DummyContent;
+
+
+public class MainAccountFragment extends Fragment {
+    private Callbacks mCallbacks;
+
+    public MainAccountFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_account, container, false);
+        rootView.findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCallbacks.onAccountSelected("one");
+            }
+        });
+        rootView.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCallbacks.onAccountSelected("two");
+            }
+        });
+        return rootView;
+    }
+
+    public interface Callbacks {
+        /**
+         * Callback for when an item has been selected.
+         */
+        void onAccountSelected(String id);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // Activities containing this fragment must implement its callbacks.
+        if (!(context instanceof Callbacks)) {
+            throw new IllegalStateException("Context must implement fragment's callbacks.");
+        }
+
+        mCallbacks = (Callbacks) context;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        mCallbacks.onAccountSelected("default");
+    }
+}
